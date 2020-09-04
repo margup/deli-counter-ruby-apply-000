353b09af5462f2a4822512a04a518cbd338ba394
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-04 18:43:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,10 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-end
+# def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+# end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
